560a768de6b7a7fc780ceaf9c3c222fd192097f4
Julien Roland <juroland@gmail.com>
2016-12-05 14:07:00 +0100

    ...

--- pyh5diff.py
+++ pyh5diff.py
@@ -3,19 +3,6 @@ import numpy as np
 import argparse
 from sklearn import preprocessing
 
-parser = argparse.ArgumentParser(description='HDF5 normalized diff')
-
-parser.add_argument('file1', help='file name of the first HDF5 file')
-parser.add_argument('file2', help='file name of the second HDF5 file')
-parser.add_argument('-p', '--precision', type=float, default=0,
-                    help='print difference if (|a-b| > PRECISION)')
-
-args = parser.parse_args()
-
-fd1 = h5py.File(args.file1, 'r')
-fd2 = h5py.File(args.file2, 'r')
-precision = args.precision
-
 def datasets_from_file(fd):
     """ Returns all datasets (name) contained in the file described in fd """
     datasets = []
@@ -27,22 +14,43 @@ def datasets_from_file(fd):
 
     return datasets
 
-datasets = datasets_from_file(fd1)
+def h5diff(filename1, filename2, precision):
+    fd1 = h5py.File(filename1, 'r')
+    fd2 = h5py.File(filename2, 'r')
 
-min_max_scaler = preprocessing.MinMaxScaler()
+    datasets = datasets_from_file(fd1)
 
-for dataset in datasets:
-    x = fd1[dataset][:].reshape(-1, 1)
-    y = fd2[dataset][:].reshape(-1, 1)
-    if np.issubdtype(fd1[dataset].dtype, np.integer):
-        x_scaled = x
-        y_scaled = y
-    else:
-        x_scaled = min_max_scaler.fit_transform(x)
-        y_scaled = min_max_scaler.fit_transform(y)
+    min_max_scaler = preprocessing.MinMaxScaler()
 
-    diff = np.fabs(x_scaled - y_scaled)
-    i = np.argmax(diff)
-    if  diff[i] > precision:
-        print('# DIFF : (dataset: {}, id: {}'
-              ', x (file1): {}, y (file2): {})'.format(dataset, i, x[i], y[i]))
+    for dataset in datasets:
+        x = fd1[dataset][:].reshape(-1, 1)
+        y = fd2[dataset][:].reshape(-1, 1)
+        if np.issubdtype(fd1[dataset].dtype, np.integer):
+            x_scaled = x
+            y_scaled = y
+        else:
+            x_scaled = min_max_scaler.fit_transform(x)
+            y_scaled = min_max_scaler.fit_transform(y)
+
+        diff = np.fabs(x_scaled - y_scaled)
+        i = np.argmax(diff)
+        if  diff[i] > precision:
+            print('# DIFF : (dataset: {}, id: {}'
+                  ', x (file1): {}'
+                  ', y (file2): {})'.format(dataset, i, x[i], y[i]))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='HDF5 normalized diff')
+
+    parser.add_argument('file1', help='file name of the first HDF5 file')
+    parser.add_argument('file2', help='file name of the second HDF5 file')
+    parser.add_argument('-p', '--precision', type=float, default=0,
+                    help='print difference if (|a-b| > PRECISION)')
+
+    args = parser.parse_args()
+
+    filename1 = args.file1
+    filename2 = args.file2
+    precision = args.precision
+
+    h5diff(filename1, filename2, precision)
